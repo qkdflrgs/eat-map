e097dd1b48b93eff380e528f455843a935d1c65f
qkdflrgs <qkdflrgs@gmail.com>
2024-04-17 23:05:06 +0900

    feat(marker): add Markers and StoreBox to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import * as stores from "@/data/store_data.json";
 import Map from "@/components/Map";
+import { useState } from "react";
+import Markers from "@/components/Markers";
+import StoreBox from "@/components/StoreBox";
 
 export default function Home() {
-  return <Map />;
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeData = stores.DATA;
+  return (
+    <>
+      <Map setMap={setMap} />
+      <Markers
+        map={map}
+        storeData={storeData}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox store={currentStore} setStore={setCurrentStore} />
+    </>
+  );
 }
