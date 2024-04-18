4e283f82aa2846780889df9f03bd267c8804fc36
qkdflrgs <qkdflrgs@gmail.com>
2024-04-18 23:52:08 +0900

    feat(fetching): add getStaticProps to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,32 @@ import Map from "@/components/Map";
 import { useState } from "react";
 import Markers from "@/components/Markers";
 import StoreBox from "@/components/StoreBox";
+import { StoreType } from "@/interface";
 
-export default function Home() {
+interface HomeProps {
+  stores: StoreType[];
+}
+
+export default function Home({ stores }: HomeProps) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
-  const storeData = stores.DATA;
+
   return (
     <>
       <Map setMap={setMap} />
-      <Markers
-        map={map}
-        storeData={storeData}
-        setCurrentStore={setCurrentStore}
-      />
+      <Markers map={map} stores={stores} setCurrentStore={setCurrentStore} />
       <StoreBox store={currentStore} setStore={setCurrentStore} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const stores = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/stores`
+  ).then((res) => res.json());
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
