4847826aaa41dbd20672cb61b150125154480ee8
qkdflrgs <qkdflrgs@gmail.com>
2024-04-24 19:25:45 +0900

    feat(react-query): set React query and devtools to App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
