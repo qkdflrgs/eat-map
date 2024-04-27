92577f549ca5b348a03a74c1bcd65b6c9e596706
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-04-28 02:06:50 +0900

    feat(next auth): add Next-auth for social login (#10)
    * feat(next-auth): install next-auth package
    
    * feat(next-auth): add Login schema to prisma
    
    * feat(next-auth): add prisma migration sql
    
    * feat(next-auth): remove unusing api file
    
    * feat(next-auth): set middleware for matcher
    
    * feat(next-auth): add session to Navbar component
    
    * feat(next-auth): add session provider to App
    
    * feat(next-auth): set Next-auth for Google login, Naver login
    
    * feat(next-auth): add Login Page

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,7 +1,51 @@
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { AiOutlineGoogle } from "react-icons/ai";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { SiNaver } from "react-icons/si";
+
 export default function LoginPage() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") router.replace("/");
+  }, [router, status]);
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex flex-col justify-center px-6 lg:px-8 h-[60vh]">
+      <div className="mx-auto w-full max-w-sm">
+        <h1 className="text-blue-800 text-center text-2xl font-semibold italic">
+          EAT-MAP
+        </h1>
+        <div className="text-center mt-6 text-2xl font-bold text-gray-600">
+          SNS 계정으로 로그인해주세요
+        </div>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          계정이 없다면 자동으로 회원가입이 진행됩니다.
+        </p>
+      </div>
+      <div className="mt-10 mx-auto w-full max-w-sm">
+        <div className="flex flex-col gap-2">
+          <button
+            type="button"
+            className="text-white flex gap-4 bg-[#4285F4] hover:bg-[#4285F4]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+            onClick={() => signIn("google", { callbackUrl: "/" })}
+          >
+            <AiOutlineGoogle className="w-6 h-6" />
+            Sign in with Google
+          </button>
+          <button
+            type="button"
+            className="text-white flex gap-5 bg-[#2db400] hover:bg-[#2db400]/90 font-medium rounded-lg w-full px-5 py-4 text-center items-center justify-center"
+            onClick={() => signIn("naver", { callbackUrl: "/" })}
+          >
+            <SiNaver className="w-4 h-4" />
+            Sign in with Naver
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
