b7a45efe2b3fc764ca483889417d120fd0318982
qkdflrgs <qkdflrgs@gmail.com>
2024-04-29 21:51:52 +0900

    feat(detail-page): add Store detail page

---
+++ src/pages/stores/[id]/StoreDetailPage.tsx
@@ -0,0 +1,121 @@
+import Loader from "@/components/Loader";
+import { StoreType } from "@/interface";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function StoreDetailPage() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [map, setMap] = useState(null);
+
+  const fetchStore = async () => {
+    const { data } = await axios(`/api/stores?id=${id}`);
+
+    return data as StoreType;
+  };
+
+  const {
+    data: store,
+    isFetching,
+    isSuccess,
+    isError,
+  } = useQuery({
+    queryKey: [`store-${id}`],
+    queryFn: fetchStore,
+    enabled: !!id,
+  });
+
+  if (isError) {
+    return (
+      <div className="w-full h-screen mx-auto pt-[10%] text-red-500 text-center font-semibold">
+        다시 시도해주세요
+      </div>
+    );
+  }
+
+  if (isFetching) {
+    return <Loader className="mt-[20%]" />;
+  }
+
+  return (
+    <>
+      <div className="max-w-5xl mx-auto px-4 py-8">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-base font-semibold leading-7 text-gray-900">
+            {store?.name}
+          </h3>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+            {store?.address}
+          </p>
+        </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                카테고리
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.category}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                주소
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.address}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                위도
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.lat}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                경도
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.lng}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                연락처
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.phone}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                식품 인증 구분
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.foodCertifyName}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">
+                업종명
+              </dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                {store?.storeType}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+      {isSuccess && (
+        <div className="overflow-hidden w-full mb-20 max-w-5xl mx-auto max-h-[600px]">
+          {/* <Map /> */}
+        </div>
+      )}
+    </>
+  );
+}
