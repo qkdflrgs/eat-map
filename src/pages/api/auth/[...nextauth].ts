b288125ab153e216c58a90f934fd6585c5940441
qkdflrgs <qkdflrgs@gmail.com>
2024-04-30 01:57:05 +0900

    feat(list): add session to authOptions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,6 +7,11 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 const prisma = new PrismaClient();
 
 export const authOptions = {
+  session: {
+    strategy: "jwt" as const,
+    maxAge: 60 * 60 * 24,
+    updateAge: 60 * 60 * 2,
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
