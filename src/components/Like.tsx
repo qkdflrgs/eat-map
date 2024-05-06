2618b0b39396d24132b8d9026a97f873ff33db9b
qkdflrgs <qkdflrgs@gmail.com>
2024-05-06 23:08:42 +0900

    feat(likes): add Like component

---
+++ src/components/Like.tsx
@@ -0,0 +1,55 @@
+import { StoreType } from "@/interface";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { toast } from "react-toastify";
+
+interface LikeProps {
+  storeId: number;
+}
+
+export default function Like({ storeId }: LikeProps) {
+  const { data: session } = useSession();
+
+  const fetchStore = async () => {
+    const { data } = await axios(`/api/stores?id=${storeId}`);
+
+    return data as StoreType;
+  };
+
+  const { data: store, refetch } = useQuery({
+    queryKey: [`like-store-${storeId}`],
+    queryFn: fetchStore,
+    enabled: !!storeId,
+    refetchOnWindowFocus: false,
+  });
+
+  const toggleLike = async () => {
+    if (session?.user && store) {
+      try {
+        const like = await axios.post("/api/likes", {
+          storeId: store.id,
+        });
+        if (like.status === 201) {
+          toast.success("찜 목록에 추가했습니다");
+        } else {
+          toast.warn("찜을 취소했습니다");
+        }
+        refetch();
+      } catch (e) {
+        console.log("error");
+      }
+    }
+  };
+
+  return (
+    <button type="button" onClick={toggleLike}>
+      {store?.likes?.length ? (
+        <AiFillHeart className="hover:text-red-600 focus:text-red-600 text-red-500" />
+      ) : (
+        <AiOutlineHeart className="hover:text-red-600 focus:text-red-600 text-red-500" />
+      )}
+    </button>
+  );
+}
