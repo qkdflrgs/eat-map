dd8f9592b60883e7cfe2010c9e7033911788d4f5
qkdflrgs <qkdflrgs@gmail.com>
2024-05-07 22:58:38 +0900

    feat(comments): fix Like component

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -10,7 +10,7 @@ interface LikeProps {
 }
 
 export default function Like({ storeId }: LikeProps) {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   const fetchStore = async () => {
     const { data } = await axios(`/api/stores?id=${storeId}`);
@@ -38,14 +38,16 @@ export default function Like({ storeId }: LikeProps) {
         }
         refetch();
       } catch (e) {
-        console.log("error");
+        console.log(e);
       }
+    } else if (status === "unauthenticated") {
+      toast.warn("로그인 후 이용해주세요");
     }
   };
 
   return (
     <button type="button" onClick={toggleLike}>
-      {store?.likes?.length ? (
+      {status === "authenticated" && store?.likes?.length ? (
         <AiFillHeart className="hover:text-red-600 focus:text-red-600 text-red-500" />
       ) : (
         <AiOutlineHeart className="hover:text-red-600 focus:text-red-600 text-red-500" />
