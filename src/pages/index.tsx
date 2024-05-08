847ecbbb2938a61181c5c6bcbd65914ecdcd118b
qkdflrgs <qkdflrgs@gmail.com>
2024-05-08 17:07:08 +0900

    feat(geolocation): add CurrentLocationButton to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import CurrentLocationButton from "@/components/CurrentLocationButton";
 import Map from "@/components/Map";
 import Markers from "@/components/Markers";
 import StoreBox from "@/components/StoreBox";
@@ -14,6 +15,7 @@ export default function Home({ stores }: HomeProps) {
       <Map />
       <Markers stores={stores} />
       <StoreBox />
+      <CurrentLocationButton />
     </>
   );
 }
