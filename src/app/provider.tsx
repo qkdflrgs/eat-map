88a31872b1ffb4b822b7f7dec67d744fbeb82ab9
qkdflrgs <qkdflrgs@gmail.com>
2024-05-09 05:36:49 +0900

    feat(appRouter): add provider to app router

---
+++ src/app/provider.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "@/components/Navbar";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export const NextProvider = ({ children }: Props) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>
+          {children}
+          <ToastContainer
+            autoClose={1000}
+            pauseOnFocusLoss={false}
+            pauseOnHover={false}
+          />
+          <ReactQueryDevtools />
+        </SessionProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
+};
+
+export const NextLayout = ({ children }: Props) => {
+  return (
+    <div className="layout">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
