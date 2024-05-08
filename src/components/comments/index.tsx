76bd7d4017cd79a6bce399c8d9c23c79bb3b54ea
qkdflrgs <qkdflrgs@gmail.com>
2024-05-09 05:38:30 +0900

    feat(appRouter): refactoring components for app router(add use client, next/navigation)

--- src/components/comments/index.tsx
+++ src/components/comments/index.tsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import CommentForm from "./CommentForm";
 import axios from "axios";
-import { useRouter } from "next/router";
+
 import { CommentApiResponse } from "@/interface";
 import { useQuery } from "@tanstack/react-query";
 import CommentsList from "./CommentsList";
@@ -9,12 +9,14 @@ import Pagination from "../Pagination";
 
 interface CommentsProps {
   storeId: number;
+  params?: {
+    page?: string;
+  };
 }
 
-export default function Comments({ storeId }: CommentsProps) {
+export default function Comments({ storeId, params }: CommentsProps) {
   const { status } = useSession();
-  const router = useRouter();
-  const { page = "1" }: any = router.query;
+  const page = params?.page || "1";
 
   const fetchComments = async () => {
     const { data } = await axios(
