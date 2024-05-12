830ed4e3c06c4bd9b19d31781d77327923aa7fc9
qkdflrgs <qkdflrgs@gmail.com>
2024-05-13 03:39:47 +0900

    fix next-auth

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import prisma from "@/db";
 import { NextResponse } from "next/server";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 
 export async function GET(req: Request) {
   const session = await getServerSession(authOptions);
