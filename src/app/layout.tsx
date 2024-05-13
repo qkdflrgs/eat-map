fb47dfb049182023616c5da95bbd7eaa081ce3bc
qkdflrgs <qkdflrgs@gmail.com>
2024-05-13 18:04:30 +0900

    feat(analytics): add Google analytics to layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
 import { NextLayout, NextProvider } from "./provider";
+import GoogleAnalytics from "./googleAnalytics";
 
 export const metadata: Metadata = {
   title: "Eat Map",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_ID} />
         <NextProvider>
           <NextLayout>{children}</NextLayout>
         </NextProvider>
